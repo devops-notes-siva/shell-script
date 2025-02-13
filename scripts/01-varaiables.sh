eb520616240a3b3ce5bfe6bb46ff0ef536f62634
siva <devops.notes.siva@gmail.com>
2025-02-13 08:40:02 +0530

    msh

---
+++ scripts/01-varaiables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON2: hi $PERSON1, how are you?"
+echo "$PERSON1: i am good, what about you $PERSON2?"
+echo "$PERSON2: i am also good , learning devops $PERSON1"
+echo "$PERSON1: from where yiu are learnig $PERSON1"
+echo "$PERSON2: from online $PEROSN1"
