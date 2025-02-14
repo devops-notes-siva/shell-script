7df0fc9934da4b8839a8c0dfd6472439b67c07ab
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-14 23:23:20 +0530

    some msg

--- scripts/7-install.sh
+++ scripts/7-install.sh
@@ -2,9 +2,9 @@
 echo "need to install some packages"
 echo "checking user is sudo previliged or not"
 
-ID=$(id -u)
+ID=${id -u}
 
-if [ $ID -ne 0]
+if [ $ID -ne 0 ]
 then
     echo "user does not have previliges to install packages"
     exit 1
