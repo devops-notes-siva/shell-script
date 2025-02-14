de99612474381e2717ea72350c81eef92335d7ff
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-14 22:24:44 +0530

    some msg

--- scripts/06-conditions.sh
+++ scripts/06-conditions.sh
@@ -2,9 +2,10 @@
 NUMBER=$1
 echo "given number is: $NUMBER"
 
-if [$NUMBER -gt 27]
+if [ $NUMBER -gt 27 ]
     echo "given number is greter than 27"
 else 
+
     echo "given number is less than 27"
  
 fi
