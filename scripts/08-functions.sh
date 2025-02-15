46f9105d6c2ef265ea5772d5464a121e1d46ea19
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-15 11:30:20 +0530

    some msg

---
+++ scripts/08-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+ echo "please use sudo acces"
+ exit 1
+fi
+
+CHECK() {
+
+if [ $1 -ne 0 ]
+then
+    echo "$2 FAILED"
+    exit 1
+else
+    echo "$2 SUCCESS"
+if
+}
+
+dnf install mysql -y
+CHECK $? "mysqll status - "
+
+dnf install git -y
+CHECK $? "GIT status - "
