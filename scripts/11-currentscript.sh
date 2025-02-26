7f4dd66fe4eeaba104105316040e9e4d2c51b200
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-26 21:09:05 +0530

    some msg

--- scripts/11-currentscript.sh
+++ scripts/11-currentscript.sh
@@ -7,8 +7,8 @@ echo  "process id of current scrip is : $$"
 
 
 echo "##########################################"
-sh 12-otherscript.sh
-
+#sh 12-otherscript.sh
+source 12-otherscript.sh
 
 echo "##########################################"
 echo "This from current script"
