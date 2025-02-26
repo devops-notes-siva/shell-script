d5a7a451de29953bcbb8bb97033aa468fd8b8167
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-26 21:06:09 +0530

    some msg

--- scripts/11-currentscript.sh
+++ scripts/11-currentscript.sh
@@ -3,8 +3,12 @@
 echo "This from current script"
 course=devops-current
 echo "this varaible is from current $course"
+echo  "process id of current scrip is : $$"
 
 sh 12-otherscript.sh
 
+
+echo "##########################################"
 echo "This from current script"
 echo "this varaible is from current $course"
+echo  "process id of current scrip is : $$"
--- scripts/12-otherscript.sh
+++ scripts/12-otherscript.sh
@@ -3,3 +3,4 @@
 echo "This from other  script"
 course=devops-other
 echo "this varaible is from current $course"
+echo  "process id of other scrip is : $$"
