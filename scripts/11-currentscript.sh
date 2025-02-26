d239ae533a6be152ef71756d79088a3a7e018f9a
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-26 21:07:01 +0530

    some msg

--- scripts/11-currentscript.sh
+++ scripts/11-currentscript.sh
@@ -5,6 +5,8 @@ course=devops-current
 echo "this varaible is from current $course"
 echo  "process id of current scrip is : $$"
 
+
+echo "##########################################"
 sh 12-otherscript.sh
 
 
@@ -12,3 +14,5 @@ echo "##########################################"
 echo "This from current script"
 echo "this varaible is from current $course"
 echo  "process id of current scrip is : $$"
+
+echo "##########################################"
