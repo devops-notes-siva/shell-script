1cac73d8483ba4f4d4e2934e56b483b0fa526d1e
devops-notes-siva <devops.notes.siva@gmail.com>
2025-02-26 21:04:22 +0530

    some msg

--- scripts/12-otherscript.sh
+++ scripts/12-otherscript.sh
@@ -3,8 +3,3 @@
 echo "This from other  script"
 course=devops-other
 echo "this varaible is from current $course"
-
-#sh 12-otherscript.sh
-
-echo "This from other script"
-echo "this varaible is from current $course"
